9406741d38d678ba9490dd1a530ca66429529bed
Vadim Lipakov <revalier01@mail.ru>
2018-03-26 19:05:58 +0300

    - Создан каркас spring mvc.

---
+++ src/main/java/com/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HelloWorldController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printHello(ModelMap model)
+            throws Exception {
+        model.addAttribute("msg", "converter");
+        return "start";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/pages/start.jsp
@@ -0,0 +1,8 @@
+<%@ page contentType = "text/html; charset = UTF-8" %>
+<html>
+    <body>
+        <h1>Video-audio converter</h1>
+        <h2> ${msg} </h2>
+    </body>
+</html>
+
